ba0c4a62d92385e3141bbb0bd1cdee60be05996d
Chris Combs <chco@nodes.dk>
2016-12-12 11:33:43 +0100

    Initial Commit

---
+++ Fingered/Fingered.h
@@ -0,0 +1,19 @@
+//
+//  Fingered.h
+//  Fingered
+//
+//  Created by Chris on 12/12/2016.
+//  Copyright © 2016 Nodes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Fingered.
+FOUNDATION_EXPORT double FingeredVersionNumber;
+
+//! Project version string for Fingered.
+FOUNDATION_EXPORT const unsigned char FingeredVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fingered/PublicHeader.h>
+
+
